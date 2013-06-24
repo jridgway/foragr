27832b35cbc038c7d297504b49cdda7623098a1a
edblue71 <em@4good.org>
2013-06-24 12:53:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foragr::Application.config.secret_key_base = '1270780633e8653298a822a784c2a961896fcdbf67233845bc82bd698d7b738ddd93a50c863a32c98ab30614fe73ea05f49d97de3d1a2cca6eb5fc82628eff77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foragr::Application.config.session_store :cookie_store, key: '_foragr_session'
